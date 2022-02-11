cdc557e9ba07977f2a2c3963ff55ea0a598b5f90
Katie Pundt <kzpundt@gmail.com>
2022-02-11 13:57:31 -0800

    update EditKegFrom to use props

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
 
 function EditKegForm(props) {
+  const {keg} = props;
+
+  function handleEditKegFormSubmission(event) {
+    event.preventDefault();
+    props.onEditKeg({
+      name: event.target.name.value,
+      abv: event.target.abv.value,
+      price: event.target.price.value,
+      brand: event.target.brand.value,
+      quantity: event.target.quantity.value,
+      id: keg.id
+    });
+  }
+
   return (
     <React.Fragment>
-      <ReusableForm 
+      <ReusableForm
+      formSubmissionHandler = {handleEditKegFormSubmission}
       buttonText="Update Keg" />
     </React.Fragment>
   );
+}
+
+EditKegForm.propTypes = {
+  keg: PropTypes.object,
+  onEditKeg: PropTypes.func
 }
 
 export default EditKegForm;

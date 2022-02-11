3c369466868a4908ebce29280e22b00677ef1fd2
Katie Pundt <kzpundt@gmail.com>
2022-02-11 14:44:55 -0800

    add header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function Header(){
+  const headerStyles = {
+    fontFamily: 'Roboto',
+    fontSize: '2em',
+    textAlign: 'center'
+  }
   return(
     <React.Fragment>
-      <h1>Tap Room</h1>
+      <div style={headerStyles}>
+        <h1>Tap Room</h1>
+      </div>
     </React.Fragment>
   );
 }
